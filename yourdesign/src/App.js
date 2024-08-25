e5345e72233abd374d94d64d658714ab23d4ccb1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-25 17:23:23 +0300

    Update App.js

--- yourdesign/src/App.js
+++ yourdesign/src/App.js
@@ -1,10 +1,14 @@
-
+import { Route, Router } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+          
+      </Router>
+
+
     </div>
   );
 }
