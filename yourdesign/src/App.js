f6fb3f3ae312f361ccec05b22b9eb3c2abc12939
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-25 18:20:06 +0300

    update Home page

--- yourdesign/src/App.js
+++ yourdesign/src/App.js
@@ -1,11 +1,12 @@
 import { Route, Router } from 'react-router-dom'
+import { Home } from './Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-          
+          <Route path="/home" element={<Home />} />
       </Router>
 
 
