b627fee1cc5712450ce5b31ce6d8fd1e6aebb5ae
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-01 12:57:58 +0300

    update react dom addons

--- yourdesign/src/App.js
+++ yourdesign/src/App.js
@@ -1,17 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 import { Home } from './components/Home/Home';
-import { UserLogin } from './components/Client/Login/Login'
+import { UserLogin } from './components/Client/Login/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div className='main-content'>
       <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/user-login" element={<UserLogin />} />
       </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
